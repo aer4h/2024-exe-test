6c57cd226ecc38381f75863c29ffb68c3889331b
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-05-04 16:53:52 +0800

    Update g.lua

--- g.lua
+++ g.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108.433, -86.61, 347.152)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-6769.63232421875, -76.09564208984357,2641.26611328125)
